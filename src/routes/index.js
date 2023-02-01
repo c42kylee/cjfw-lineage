88bacdd6483cd3dde44b38379f8c8594cab1ae84
kylee <kylee@cornerstone42.co.kr>
2023-02-01 15:31:56 +0900

    set init

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import styles from './Routes.module.scss'
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <h1>HI, CJFW-LINEAGE</h1>
+    </div>
+  )
+}
+
+export default App
